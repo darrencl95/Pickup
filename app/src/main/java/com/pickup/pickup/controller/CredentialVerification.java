8dd9ac71e1dc1f919bebfdf055798f23edacaa2a
ZSchlesinger <zach.schlesinger@gmail.com>
2017-03-04 14:41:16 -0500

    Add password verification

--- app/src/main/java/com/pickup/pickup/controller/CredentialVerification.java
+++ app/src/main/java/com/pickup/pickup/controller/CredentialVerification.java
@@ -1,7 +1,6 @@
 package com.pickup.pickup.controller;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import android.widget.Toast;
 
 /**
  * Created by zachschlesinger on 3/4/17.
@@ -9,15 +8,23 @@ import java.util.regex.Pattern;
 
 public class CredentialVerification {
 
-    public static final Pattern p = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x07\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x07\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
-
-    public static boolean verifyEmail(String email) {
-        Matcher m = p.matcher(email);
-        return m.matches();
-    }
-
     // passwords require uppercase, lowercase, and numbers. minimum of 8 characters
     // emails require string + @ + string + . + string
 
+    private static String verifyPassword(String password) {
+        String message;
+        boolean length = password.length() >= 8;
+        boolean hasUppercase = !password.equals(password.toLowerCase());
+        boolean hasLowercase = !password.equals(password.toUpperCase());
+        boolean hasNumber = password.matches(".*\\d+.*");
+
+        message = (!length) ? "Password is too short" : null;
+        message = (!hasUppercase) ? "Password does not contain an uppercase" : null;
+        message = (!hasLowercase) ? "Password does not contain an owercase" : null;
+        message = (!hasNumber) ? "Password does not contain a number" : null;
+
+
+        return message;
+    }
 
 }
