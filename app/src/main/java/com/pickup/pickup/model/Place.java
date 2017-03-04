6eb438febbb56e5a8d89397bb752a138589ea0c9
ZSchlesinger <zach.schlesinger@gmail.com>
2017-03-04 13:18:26 -0500

    add user, place, and event model classes

---
+++ app/src/main/java/com/pickup/pickup/model/Place.java
@@ -0,0 +1,8 @@
+package com.pickup.pickup.model;
+
+/**
+ * Created by zachschlesinger on 3/4/17.
+ */
+
+public class Place {
+}
---
+++ app/src/main/java/com/pickup/pickup/model/User.java
@@ -0,0 +1,13 @@
+package com.pickup.pickup.model;
+
+/**
+ * Created by zachschlesinger on 3/4/17.
+ */
+
+public class User {
+
+    private String username;
+    private String firstName;
+    private String lastName;
+
+}
