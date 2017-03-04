76bf53a7bfd357dc82ff20d4e86692a7f28ca8dd
ZSchlesinger <zach.schlesinger@gmail.com>
2017-03-04 14:14:01 -0500

    add credentialverification class

---
+++ app/src/main/java/com/pickup/pickup/controller/CredentialVerification.java
@@ -0,0 +1,13 @@
+package com.pickup.pickup.controller;
+
+/**
+ * Created by zachschlesinger on 3/4/17.
+ */
+
+public class CredentialVerification {
+
+    // passwords require uppercase, lowercase, and numbers. minimum of 8 characters
+    // emails require string + @ + string + . + string
+
+
+}
