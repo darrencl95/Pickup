c9b05151d7ab747585d85f2e3fd0a30070125471
ZSchlesinger <zach.schlesinger@gmail.com>
2017-03-10 22:36:12 -0500

    add nav bar fragment and implement click listeners

---
+++ app/src/main/java/com/pickup/pickup/controller/NavigationActivity.java
@@ -0,0 +1,75 @@
+package com.pickup.pickup.controller;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.pickup.pickup.R;
+
+public class NavigationActivity extends Fragment {
+
+    private Button buttonProfile;
+    private Button buttonFriends;
+    private Button buttonCurrent;
+    private Button buttonPast;
+    private Button buttonSettings;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.navigation_view, container, false);
+        buttonProfile = (Button) rootView.findViewById(R.id.buttonProfile);
+        buttonFriends = (Button) rootView.findViewById(R.id.buttonFriends);
+        buttonCurrent = (Button) rootView.findViewById(R.id.buttonCurrent);
+        buttonPast = (Button) rootView.findViewById(R.id.buttonPast);
+        buttonSettings = (Button) rootView.findViewById(R.id.buttonSettings);
+
+//        buttonProfile.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(getActivity(), "Profile button pressed", Toast.LENGTH_SHORT).show();
+//                Log.d("fragment", "button profile onclick works");
+//            }
+//        });
+        buttonProfile.setOnClickListener(clickListener);
+        buttonFriends.setOnClickListener(clickListener);
+        buttonCurrent.setOnClickListener(clickListener);
+        buttonPast.setOnClickListener(clickListener);
+        buttonSettings.setOnClickListener(clickListener);
+
+
+        return rootView;
+    }
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.buttonProfile:
+                    Toast.makeText(getActivity(), "Profile button pressed", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.buttonFriends:
+                    Toast.makeText(getActivity(), "Friends button pressed", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.buttonCurrent:
+                    Toast.makeText(getActivity(), "Current button pressed", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.buttonPast:
+                    Toast.makeText(getActivity(), "Past button pressed", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.buttonSettings:
+                    Toast.makeText(getActivity(), "Settings button pressed", Toast.LENGTH_SHORT).show();
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+
+
+}
