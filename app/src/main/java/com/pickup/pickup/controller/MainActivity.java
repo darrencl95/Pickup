ffa54c2ac458386245587555b5d4a30f1c37b5ca
Leung, Darren C <darren.leung@gatech.edu>
2017-03-10 22:46:21 -0500

    Consolidate Main and Maps activity

--- app/src/main/java/com/pickup/pickup/controller/MainActivity.java
+++ app/src/main/java/com/pickup/pickup/controller/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_main);
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkLocationPermission();
         }
