a4a50255687094bbcb7df0c3fd7d683206e4d09c
ZSchlesinger <zach.schlesinger@gmail.com>
2017-03-15 11:30:33 -0400

    longpress listener for profile picture. takes you to view to select from gallery

--- app/src/main/java/com/pickup/pickup/controller/ProfileActivity.java
+++ app/src/main/java/com/pickup/pickup/controller/ProfileActivity.java
@@ -1,7 +1,15 @@
 package com.pickup.pickup.controller;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.pickup.pickup.R;
 
@@ -10,9 +18,58 @@ import com.pickup.pickup.R;
  */
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private GestureDetectorCompat mDetector;
+    private ImageView profileImage;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        profileImage = (ImageView) findViewById(R.id.imageView);
+
+        profileImage.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Toast.makeText(getBaseContext(), "Long press ayyy", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(ProfileActivity.this, GalleryActivity.class));
+                return true;
+            }
+        });
+
+        // instantiate gesture detector
+        mDetector = new GestureDetectorCompat(getBaseContext(), new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent motionEvent) {
+                return false;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent motionEvent) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent motionEvent) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent motionEvent) {
+                Toast.makeText(getBaseContext(), "Long press ayyy", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+                return false;
+            }
+        });
     }
 }
