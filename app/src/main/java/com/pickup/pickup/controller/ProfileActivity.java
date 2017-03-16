4e83fb8e945dc78dfdecff5d8e112e827c62204a
ZSchlesinger <zach.schlesinger@gmail.com>
2017-03-16 16:03:41 -0400

    store profile picture in firebase storage and set profile imageview with image from storage

--- app/src/main/java/com/pickup/pickup/controller/ProfileActivity.java
+++ app/src/main/java/com/pickup/pickup/controller/ProfileActivity.java
@@ -1,17 +1,30 @@
 package com.pickup.pickup.controller;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.view.GestureDetectorCompat;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
-import android.widget.Gallery;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.pickup.pickup.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Created by zachschlesinger on 3/13/17.
@@ -19,8 +32,9 @@ import com.pickup.pickup.R;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private GestureDetectorCompat mDetector;
     private ImageView profileImage;
+    private StorageReference mStorageRef;
+    private FirebaseUser firebaseUser;
 
 
     @Override
@@ -28,48 +42,95 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         profileImage = (ImageView) findViewById(R.id.imageView);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        StorageReference userProf = mStorageRef.child(firebaseUser.getUid());
+        try {
+            final File localFile = File.createTempFile(firebaseUser.getUid().toString(), "png");
+            userProf.getFile(localFile)
+                    .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            Log.d("firebase_file_path", localFile.getAbsolutePath());
+                            Bitmap bmp = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                            profileImage.setImageBitmap(bmp);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            profileImage.setImageResource(R.drawable.default_profile);
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        StorageReference defaultRef = mStorageRef.child("default/default.png");
+//        Uri path = Uri.parse("android.resource://com.pickup.pickup/" + R.drawable.default_profile);
+//        defaultRef.putFile(path)
+//                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                    @Override
+//                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                        Toast.makeText(getBaseContext(), "this worked! :)", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.e("FIREBASE_STORAGE", e.getMessage().toString());
+//                        Toast.makeText(getBaseContext(), "this didn't work", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
 
         profileImage.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View view) {
-                Toast.makeText(getBaseContext(), "Long press ayyy", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(ProfileActivity.this, GalleryActivity.class));
+                imageOnClick();
                 return true;
             }
         });
+    }
 
-        // instantiate gesture detector
-        mDetector = new GestureDetectorCompat(getBaseContext(), new GestureDetector.OnGestureListener() {
-            @Override
-            public boolean onDown(MotionEvent motionEvent) {
-                return false;
+    private void imageOnClick() {
+        Intent intent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 0);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK){
+            Uri targetUri = data.getData();
+//            textTargetUri.setText(targetUri.toString());
+//            StorageReference darrenProf = mStorageRef.child("darrenProfilePicture/profile.jpg");
+
+            Bitmap bitmap;
+            try {
+                bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(targetUri));
+//                profileImage.setImageBitmap(bitmap);
+                StorageReference userProf = mStorageRef.child(firebaseUser.getUid());
+                userProf.putFile(targetUri)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                Toast.makeText(getBaseContext(), "this worked! :)", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e("FIREBASE_STORAGE", e.getMessage().toString());
+                                Toast.makeText(getBaseContext(), "this didn't work", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
-
-            @Override
-            public void onShowPress(MotionEvent motionEvent) {
-
-            }
-
-            @Override
-            public boolean onSingleTapUp(MotionEvent motionEvent) {
-                return false;
-            }
-
-            @Override
-            public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-                return false;
-            }
-
-            @Override
-            public void onLongPress(MotionEvent motionEvent) {
-                Toast.makeText(getBaseContext(), "Long press ayyy", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public boolean onFling(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-                return false;
-            }
-        });
+        }
     }
 }
